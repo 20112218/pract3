c312702e962fec97d0a7df0725f0c85e9db23ef6
KUNAL KULDIPKE <128902355+20112218@users.noreply.github.com>
2023-09-11 14:45:46 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,1 +1,6 @@
-print ("hhhjhhhhhhh")
+user_name = input("Enter your name: ")
+user_email = input("Enter your email address: ")
+
+# Print the user's name and email
+print("Your name is:", user_name)
+print("Your email address is:", user_email)
